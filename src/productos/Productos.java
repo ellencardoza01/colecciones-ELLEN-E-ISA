745ef73680271e2e2335aebc153412e54b2c2638
HP <HP@Cristal-Cardozo>
2026-09-22 17:14:05 -0500

    CLASE PRODUCTO ISA Y ELLEN

---
+++ src/productos/Productos.java
@@ -0,0 +1,49 @@
+package productos;
+public class Productos {
+
+       private int id;
+       private String nombre;
+       private double precio;
+       
+    public Productos() {
+    }
+    public Productos(int id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" + "id=" + id + ", nombre=" + nombre + ", precio=" + precio + '}';
+    }
+
+    
+       
+  
+    
+}
